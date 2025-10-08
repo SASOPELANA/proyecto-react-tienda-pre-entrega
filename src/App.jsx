7573a3ad32e45243ab8ee6395d8d4020156dd481
Sergio Dev <sopekof@gmail.com>
2025-10-08 17:54:10 -0300

    Proyecto pre entrega React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LogoCarrito from "../public/icon-carrito.png";
+
+function App() {
+  return (
+    <main className="font-montserrat">
+      <div>Mi proyecto pre entrega</div>
+      <img src={LogoCarrito} alt="logo carrito" />
+      <h1>Carrito de compras</h1>
+    </main>
+  );
+}
+
+export default App;
